9c07bf7f1f4182a26c9638da4703ffc8164f9b76
Serina0804 <miyake@ailab.ics.keio.ac.jp>
2024-12-11 17:55:23 +0900

    change port

--- app.py
+++ app.py
@@ -82,4 +82,8 @@ def delete_record(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Renderの環境変数PORTを取得し、デフォルト値は5000
+    port = int(os.environ.get("PORT", 5000))
+    # ホストを0.0.0.0に設定して、すべてのネットワークインターフェースからのリクエストを受け入れる
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
